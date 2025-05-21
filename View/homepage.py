0f127f8745b0145da044a57f0cf3addb7696ab91
kozemale <ahmadtoran@googlemail.com>
2025-05-21 16:29:11 +0200

    testversuch nr. 6

--- View/homepage.py
+++ View/homepage.py
@@ -8,7 +8,7 @@ class HomePage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.evaluation = GPSBackendSignalMessung()
-        self.controller = GPSController()
+        self.controller = GPSController(self)
 
 
         #Aufteilung Seite in 2
