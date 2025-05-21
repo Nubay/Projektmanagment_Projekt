d0410dafa216471257c7d9ae7265281d7c1a9364
kozemale <ahmadtoran@googlemail.com>
2025-05-21 17:09:54 +0200

    testversuch nr. 10

--- Controller/controller.py
+++ Controller/controller.py
@@ -7,4 +7,4 @@ class GPSController:
 
     def submit_data(self, daten: Tuple[float, float]):
         self.homepage.show_gps_data(f"Lat: {daten[0]:.5f}, Lon: {daten[1]:.5f}")
-
+        self.homepage.show_gps_data( '13345666432211 r')
