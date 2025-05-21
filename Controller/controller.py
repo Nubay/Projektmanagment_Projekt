039c7c449502e9ee4affb01a6e5c7eaf425db205
kozemale <ahmadtoran@googlemail.com>
2025-05-21 16:48:40 +0200

    testversuch nr. 8

--- Controller/controller.py
+++ Controller/controller.py
@@ -1,12 +1,11 @@
 from typing import Tuple
-from View.homepage import HomePage
+
 
 class GPSController:
-    def __init__(self):
-        self.homepage = HomePage(self)
-
     
     def submit_data(self, daten: Tuple[float, float]):
+        from View.homepage import HomePage
+        self.homepage = HomePage(self)
         gps_data = daten
         self.homepage.show_gps_data(gps_data)
 
